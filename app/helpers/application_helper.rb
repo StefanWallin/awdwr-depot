44fe017c3864f194a51b7de57c40cf381a848768
Stefan Wallin <stefan.w@festiz.com>
2012-12-05 21:58:42 +0100

    8.3: Created catalog listing, Added page layout, Used a predefined price helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    # def property(sym)
+    #     define_method(sym) do
+    #         instance_variable_get("@#{sym}")
+    #     end
+    #     define_method("#{sym}=") do |value|
+    #         instance_variable_set("@#{sym}",value)
+    #     end
+    # end
+
 end
