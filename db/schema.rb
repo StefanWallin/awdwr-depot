ecab641514a18545cb14775a725ee931f7da0838
Stefan Wallin <stefan.w@festiz.com>
2012-12-15 16:30:43 +0100

    9.1: Create cart stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121127201958) do
+ActiveRecord::Schema.define(:version => 20121208193137) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "products", :force => true do |t|
     t.string   "title"
